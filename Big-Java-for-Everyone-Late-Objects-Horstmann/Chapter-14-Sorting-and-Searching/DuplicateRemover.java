a225dc71a6c9da59c0be79ea88c814f95a2d2e52
syndbg <syndbe@gmail.com>
2015-02-14 22:40:11 +0200

    Updated DuplicateRemover

--- Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-14-Sorting-and-Searching/DuplicateRemover.java
+++ Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-14-Sorting-and-Searching/DuplicateRemover.java
@@ -1,15 +1,17 @@
-import java.util.ArrayList;
+package chapter14;
+
+import java.util.Arrays;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 public class DuplicateRemover {
-    // NAIVE SOLUTION 0(n^2)
+    // with auxiliary array
     public static int[] removeDuplicates(int[] array) {
-        List<Integer> output = new ArrayList<Integer>();
+        int[] output = new int[array.length];
+        int addPos = 0;
         for (int i = 0; i < array.length; i++) {
             boolean hasDuplicate = false;
-            int j = i +1;
+            int j = i + 1;
             while (!hasDuplicate && j < array.length) {
                 if (array[i] == array[j]) {
                     hasDuplicate = true;
@@ -17,30 +19,42 @@ public class DuplicateRemover {
                 j += 1;
             }
             if (!hasDuplicate) {
-                output.add(array[i]);
+                output[addPos] = array[i];
+                addPos++;
             }
         }
-        array = new int[output.size()];
-        int i = 0;
-        for (Integer integer : output) {
-            array[i] = integer;
-            i += 1;
-        }
-        return array;
+        System.out.println(Arrays.toString(output));
+        return output;
     }
 
+    public static int[] removeDuplicatesWithoutAuxiliaryArrays(int[] array) {
+        int arraySize = array.length;
+        for (int i = 0; i < arraySize - 1; i++) {
+            for (int j = i + 1; j < arraySize; j++) {
+                if (array[i] == array[j]) {
+                    // shift over the repetitive elements
+                    for (int r = j; r < arraySize - 1; r++) {
+                        array[r] = array[r + 1];
+                    }
+                    arraySize--;
+                    j--;
+                }
+            }
+        }
+        // return array trimmed to reduced size
+        return Arrays.copyOfRange(array, 0, arraySize);
+    }
 
-    public static int[] removeDuplicatesBest(int[] array) {
+    public static int[] removeDuplicatesWithCollection(int[] array) {
         Set<Integer> set = new LinkedHashSet<Integer>();
         for (int element : array) {
             set.add(element);
         }
-        set.toArray();
         array = new int[set.size()];
         int i = 0;
         for (Integer integer : set) {
             array[i] = integer;
-            i += 1;
+            i++;
         }
         return array;
     }
