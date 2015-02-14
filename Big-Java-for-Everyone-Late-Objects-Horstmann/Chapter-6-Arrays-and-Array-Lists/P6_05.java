c5ee165672e687ddf4f1aa8c52accab60707ab2e
syndbg <syndbe@gmail.com>
2015-02-14 22:08:35 +0200

    Fixed removeMin

--- Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-6-Arrays-and-Array-Lists/P6_05.java
+++ Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-6-Arrays-and-Array-Lists/P6_05.java
@@ -1,18 +1,18 @@
+package chapter6;
+
+import java.util.Arrays;
+
 /*Write a method public static void removeMin that removes the minimum value from a
-partially filled array without calling other methods.*/
+ partially filled array without calling other methods.*/
 
 public class P6_05 {
-    public static void removeMin(int[] seq) {
-        int minElement = seq[0];
+    public static int[] removeMin(int[] seq) {
         int minIndex = 0;
         for (int i = 1; i < seq.length; i++) {
-            if (seq[i] != 0 && seq[i] < minElement) {
-                minElement = seq[i];
+            if (seq[i] < seq[minIndex]) {
                 minIndex = i;
             }
         }
-        for (int i = minIndex; i < seq.length - 1; i++) {
-            seq[i] = seq[i + 1];
-        }
+        return Arrays.copyOfRange(seq, minIndex + 1, seq.length);
     }
 }
