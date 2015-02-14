7a12c964a479725ed9dbc29e60e5bea895c51e19
syndbg <syndbe@gmail.com>
2015-02-14 15:22:05 +0200

    Updated mergeSorter with merge sum. Was asked in chapter 14 exercises/questions.

--- Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-14-Sorting-and-Searching/MergeSortDemo.java
+++ Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-14-Sorting-and-Searching/MergeSortDemo.java
@@ -1,20 +1,42 @@
-import java.util.Arrays;
-import java.util.Scanner;
+package chapter14;
 
+import java.util.Scanner;
 
 public class MergeSortDemo {
     public static void main(String[] args) {
         Scanner consoleIn = new Scanner(System.in);
-        System.out.println("Array size: ");
-        int arraySize = consoleIn.nextInt();
+        System.out.print("Smallest sample:");
+        int smallestSample = consoleIn.nextInt();
+        System.out.print("Largest sample:");
+        int largestSample = consoleIn.nextInt();
         consoleIn.close();
-        int[] arr = ArrayUtils.randomIntArray(arraySize, 100);
-        System.out.println(Arrays.toString(arr));
-        StopWatch stopwatch = new StopWatch();
+        System.out.println(generateCharts(smallestSample, largestSample));
+    }
+
+    /**
+     * Time the algorithm and return milliseconds.
+     * 
+     * @param array
+     * @param stopwatch
+     * @return
+     */
+    public static long timeSorting(int[] array, StopWatch stopwatch) {
         stopwatch.start();
-        MergeSorter.sort(arr);
+        MergeSorter.sort(array);
         stopwatch.stop();
-        stopwatch.showElapsedTime();
-        System.out.println(Arrays.toString(arr));
+        return stopwatch.getElapsedTime();
+    }
+
+    public static String generateCharts(int smallestSample, int largestSample) {
+        int[][] arrays = ArrayUtils.generateSampleArrays(smallestSample, largestSample);
+        StringBuilder result = new StringBuilder("n - milliseconds\n");
+
+        StopWatch stopwatch = new StopWatch();
+        for (int[] array : arrays) {
+            long timedSorting = timeSorting(array, stopwatch);
+            stopwatch.reset();
+            result.append(String.format("%s - %dms\n", array.length, timedSorting));
+        }
+        return result.toString();
     }
 }
--- Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-14-Sorting-and-Searching/MergeSorter.java
+++ Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-14-Sorting-and-Searching/MergeSorter.java
@@ -1,4 +1,38 @@
+package chapter14;
+
+import java.util.Arrays;
+
 public class MergeSorter {
+    public static int mergeSum(int[] array) {
+        if (array.length == 1) {
+            return array[0];
+        }
+        int[] first = Arrays.copyOfRange(array, 0, array.length / 2);
+        int[] second = Arrays.copyOfRange(array, first.length, array.length);
+        int sumFirst = mergeSum(first);
+        int sumSecond = mergeSum(second);
+        return sumFirst + sumSecond;
+    }
+
+    public static void mergeSort(int[] array) {
+        if (array.length <= 1) {
+            return;
+        }
+        int[] first = new int[array.length / 2];
+        int[] second = new int[array.length - first.length];
+
+        for (int i = 0; i < first.length; i++) {
+            first[i] = array[i];
+        }
+        for (int i = 0; i < second.length; i++) {
+            second[i] = array[i + first.length];
+        }
+
+        mergeSort(first);
+        mergeSort(second);
+        merge(first, second, array);
+    }
+
     public static void sort(int[] array) {
         if (array.length <= 1) {
             return;
