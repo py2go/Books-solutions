ee6bd973b091ec25891ecbdd8eb910f432442a43
syndbg <syndbe@gmail.com>
2015-02-14 21:06:38 +0200

    Updated sumWithoutSmallest

--- Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-6-Arrays-and-Array-Lists/P6_04.java
+++ Big-Java-for-Everyone-Late-Objects-Horstmann/Chapter-6-Arrays-and-Array-Lists/P6_04.java
@@ -1,6 +1,8 @@
+package chapter6;
+
 /*Write a method sumWithoutSmallest that computes the sum of an array of values,
-except for the smallest one, in a single loop. In the loop, update the sum and the
-smallest value. After the loop, return the difference.*/
+ except for the smallest one, in a single loop. In the loop, update the sum and the
+ smallest value. After the loop, return the difference.*/
 
 public class P6_04 {
     public static int sumWithoutSmallest(int[] seq) {
@@ -10,6 +12,7 @@ public class P6_04 {
         }
         return sum - findSmallest(seq);
     }
+
     public static int findSmallest(int[] seq) {
         int smallest = seq[0];
         for (int i = 0; i < seq.length; i++) {
@@ -18,5 +21,18 @@ public class P6_04 {
             }
         }
         return smallest;
+    }
+
+    // more efficient solution to do things at once
+    public static int sumEfficient(int[] seq) {
+        int sum = seq[0];
+        int smallest = seq[0];
+        for (int i = 1; i < seq.length; i++) {
+            if (seq[i] < smallest) {
+                smallest = seq[i];
+            }
+            sum += seq[i];
+        }
+        return sum - smallest;
     }
 }
